4baeccbf0b816e400b83bdc50bd47a1859077395
Giri.AlkondanSubbiah <girias@gmail.com>
2018-02-19 21:13:12 -0600

    fixed date

--- src/com/example/exceptionhandling/PaymentBO.java
+++ src/com/example/exceptionhandling/PaymentBO.java
@@ -1,11 +1,16 @@
 package com.example.exceptionhandling;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PaymentBO {
 
     public boolean processPayment(com.example.exceptionhandling.Cheque cheque) throws InvalidDateException {
 
-        if(cheque.getChequeDate().getTime() < new Date().getTime()) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        String chequeDate = sdf.format(cheque.getChequeDate());
+
+        String today = sdf.format(new Date());
+        if(chequeDate.compareTo(today) < 0) {
             throw new InvalidDateException("Cheque is valid only for three months");
         }
         return true;
