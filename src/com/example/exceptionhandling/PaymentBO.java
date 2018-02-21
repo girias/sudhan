c0b9c016d4a83e843a3cc56469570f59c5fd24d9
Giri.AlkondanSubbiah <girias@gmail.com>
2018-02-20 19:52:26 -0600

    fixed date

--- src/com/example/exceptionhandling/PaymentBO.java
+++ src/com/example/exceptionhandling/PaymentBO.java
@@ -1,16 +1,11 @@
 package com.example.exceptionhandling;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class PaymentBO {
 
     public boolean processPayment(Cheque cheque) throws InvalidDateException {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        String chequeDate = sdf.format(cheque.getChequeDate());
-
-        String today = sdf.format(new Date());
         int diffInDays = (int)( (new Date().getTime() - cheque.getChequeDate().getTime())
                 / (1000 * 60 * 60 * 24) );
 
