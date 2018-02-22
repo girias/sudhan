f3ddfe564bef55a94be48c18048874705abdfae1
Giri.AlkondanSubbiah <girias@gmail.com>
2018-02-21 18:42:01 -0600

    fixed the get name

--- src/com/example/threads/Main.java
+++ src/com/example/threads/Main.java
@@ -22,10 +22,10 @@ public class Main {
         itemList.add(new Item(1203L, "Lenovo Vibe K5 Note", "gohastings", 25000.0, 3.5));
         itemList.add(new Item(1204L, "Asus Zenfone 3", "deluxe-marken", 18000.0, 3.0));
         itemList.add(new Item(1205L, "Lenovo Vibe K5 Note", "kayfast1", 23500.0, 3.2));
-        itemList.add(new Item(1209L, "Samsung Galaxy On5", "deluxe-marken", 16000.0, 3.0));
         itemList.add(new Item(1206L, "Moto G5 Plus", "deluxe-marken", 16500.0, 3.8));
         itemList.add(new Item(1207L, "Asus Zenfone 3", "Paypal Digital Gifts", 18500.0, 4.6));
         itemList.add(new Item(1208L, "Lenovo Vibe K5 Note", "Paypal Digital Gifts", 24000.0, 4.1));
+        itemList.add(new Item(1209L, "Samsung Galaxy On5", "deluxe-marken", 16000.0, 3.0));
         itemList.add(new Item(1210L, "Asus Zenfone 3", "deluxe-marken", 17500.0, 3.2));
         itemList.add(new Item(1211L, "Lenovo P2", "kayfast1", 25000.0, 3.4));
         itemList.add(new Item(1212L, "Moto M", "kayfast1", 15000.0, 3.0));
@@ -43,7 +43,7 @@ public class Main {
             String[] items = reader.readLine().split(",");
             SearchProviderThread ob[] = new SearchProviderThread[items.length];
             for (int i = 0; i < items.length; i++) {
-                String name = itemList.get(Integer.parseInt(items[i]) - 1).getName();
+                String name = toDisplay.get(Integer.parseInt(items[i]) - 1);
                 System.out.println(">>>>>" + name);
                 ob[i] = new SearchProviderThread(name, name, itemList);
                 Thread.sleep(1000);
