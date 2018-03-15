061ce73b3f63b842093f1746ab18d5b3c3452679
Sudhan Alkondan Manickavasagom <sudhan.hariharan@gmail.com>
2018-03-14 22:29:37 -0500

    Just changed the System.format to Main instead of InvoiceBO

--- src/com/example/encapsulation2/InvoiceBO.java
+++ src/com/example/encapsulation2/InvoiceBO.java
@@ -4,7 +4,7 @@ package com.example.encapsulation2;
 public class InvoiceBO {
     public void displayUserDetailsByStatus(String status, Invoice[] invoiceArray) {
         // fill code here
-        System.out.format("%-15s %-15s %-15s", "Name", "State", "Country");
+        //System.out.format("%-15s %-15s %-15s", "Name", "State", "Country");
         for (Invoice invoice : invoiceArray) {
             if (invoice.getStatus().equalsIgnoreCase(status)) {
                 System.out.format("\n%-15s %-15s %-15s", invoice.getCustomer().getName(), invoice.getCustomer().getState(), invoice.getCustomer().getCountry());
