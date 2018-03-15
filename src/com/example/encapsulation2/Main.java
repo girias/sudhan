1ab3969ad15a121fe1647589e01a7f1ba73fdcfa
Giri.AlkondanSubbiah <girias@gmail.com>
2018-03-14 19:57:53 -0500

    Fixed alignment issue

--- src/com/example/encapsulation2/Main.java
+++ src/com/example/encapsulation2/Main.java
@@ -7,6 +7,27 @@ import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Sample Input and output
+ * Enter the number of invoices :
+ * 4
+ * Enter the invoice 1 details :
+ * Raj,Sri nagar,India,52670.00,Not Cleared,Ravi
+ * Enter the invoice 2 details :
+ * Emi,Kerala,India,52200.00,Not cleared,Shankar
+ * Enter the invoice 3 details :
+ * Julie,Mexico,USA,52000.69,Cleared,Williams
+ * Enter the invoice 4 details :
+ * Ram,Delhi,India,52312.20,Not cleared,Sasi
+ * Customer details with status cleared Invoices :
+ * Name            State           Country
+ * Julie           Mexico          USA
+ * Enter the Invoice creator name :
+ * Viji
+ * The total invoice value :
+ * No invoice available
+ */
+
 public class Main {
 
     public static void main(String[] args) throws NumberFormatException, IOException {
@@ -16,7 +37,7 @@ public class Main {
         // fill code here
         List<Invoice> inputData = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            System.out.println("Enter the invoice" + (i + 1) + "details :");
+            System.out.println("Enter the invoice " + (i + 1) + " details :");
             String[] input = buf.readLine().split(",");
             Invoice invoice = new Invoice(new User(input[0], input[1], input[2]), Double.parseDouble(input[3]), input[4], input[5]);
             inputData.add(invoice);
