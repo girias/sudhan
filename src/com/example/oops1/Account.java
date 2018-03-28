3018cf0c7ff710c7addbb77eae8a48cd301ed277
Giri.AlkondanSubbiah <girias@gmail.com>
2018-03-27 22:42:42 -0500

    made a separate package for oops1

---
+++ src/com/example/oops1/Account.java
@@ -0,0 +1,64 @@
+//package com.example.oops1;
+//
+//public class Account {
+//    String username;
+//    String password;
+//    int cvv;
+//    byte[] fingerPrint;
+//
+//    public Account() {
+//    }
+//
+//    public Account(String username, String password, int cvv, byte[] fingerPrint) {
+//        this.username = username;
+//        this.password = password;
+//        this.cvv = cvv;
+//        this.fingerPrint = fingerPrint;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public int getCvv() {
+//        return cvv;
+//    }
+//
+//    public void setCvv(int cvv) {
+//        this.cvv = cvv;
+//    }
+//
+//    public byte[] getFingerPrint() {
+//        return fingerPrint;
+//    }
+//
+//    public void setFingerPrint(byte[] fingerPrint) {
+//        this.fingerPrint = fingerPrint;
+//    }
+//
+//    public Boolean authentication(String username, String password) {
+//        //fill the code
+//    }
+//
+//    public Boolean authentication(String username, int cvv) {
+//        //fill the code
+//    }
+//
+//    public Boolean authentication(byte[] fingerPrint) {
+//        //fill the code
+//    }
+//
+//}
+//
---
+++ src/com/example/oops1/Main.java
@@ -0,0 +1,30 @@
+//package com.example.oops1;
+//
+//public class Main {
+//    public static void main(String args[]) {
+//        Account accountArray[] = new Account[5];
+//        accountArray[0] = new Account("vicky", "vicky123", 999, "0011".getBytes());
+//        accountArray[1] = new Account("mark", "mark007", 100, "1111".getBytes());
+//        accountArray[2] = new Account("priya", "priya332", 444, "0101".getBytes());
+//        accountArray[3] = new Account("max", "max232", 555, "0001".getBytes());
+//        accountArray[4] = new Account("martin", "martin345", 888, "0010".getBytes());
+//        System.out.println("Enter the login mode");
+//        System.out.println("1)Browser\n2)Wallet\n3)Mobile");
+//        //fill the code
+//        switch (choice) {
+//            case 1:
+//                System.out.println("Enter the username");
+//                //fill the code
+//                System.out.println("Enter the password");
+//                //fill the code
+//            case 2:
+//                System.out.println("Enter the username");
+//                //fill the code
+//                System.out.println("Enter the CVV");
+//                //fill the code
+//            case 3:
+//                System.out.println("Enter the fingerprint");
+//                //fill the code
+//        }
+//    }
+//}
